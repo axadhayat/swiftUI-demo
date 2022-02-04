4042247eb20413bb61e5bb1b2c426769af625462
Asad <axad.hayat@gmail.com>
2022-02-04 14:21:02 +0500

    First push

---
+++ swiftUI-demo/swiftUI-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI-demo
+//
+//  Created by Asad Hayat on 04/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI-demo/swiftUI-demo/swiftUI_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_demoApp.swift
+//  swiftUI-demo
+//
+//  Created by Asad Hayat on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
